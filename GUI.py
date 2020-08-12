f0b0fb0a0972fa6fc5ca93c635f95be7b92c93be
Rayaan Attari <rayaan.attari@gmail.com>
2020-08-12 19:35:28 +0530

    Added further functionality to GUI.

--- GUI.py
+++ GUI.py
@@ -7,14 +7,16 @@ class App:
         pygame.init()
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         self.running = True
-        self.grid = testBoard
+        self.grid = testBoard2
         self.selected = None
         self.mousePos = None
         self.state = "Playing"
+        self.font = pygame.font.SysFont(font, cellSize // 2)
         self.playingButtons = []
         self.menuButtons = []
         self.endButtons = []
-        self.loadButtons()
+        self.lockedCells = []
+        self.load()
 
     def run(self):
         while self.running:
@@ -37,9 +39,10 @@ class App:
                     self.selected = selected
                 else:
                     self.selected = None
-
-
-
+            if event.type == pygame.KEYDOWN:
+                if self.selected is not None and list(self.selected) not in self.lockedCells:
+                    if self.isInt(event.unicode):
+                        self.grid[self.selected[1]][self.selected[0]] = int(event.unicode)
 
     def playingUpdate(self):
         self.mousePos = pygame.mouse.get_pos()
@@ -55,33 +58,32 @@ class App:
 
         if self.selected:
             self.drawSelection(self.window, self.selected)
+
+        self.shadeLockedCells(self.window, self.lockedCells)
+
+        self.drawNumbers(self.window)
+
         self.drawGrid(self.window)
         pygame.display.update()
 
 ########## HELPER FUNCTIONS ##########
 
+    def drawNumbers(self, window):
+        for y, row in enumerate(self.grid):
+            for x, num in enumerate(row):
+                if num != 0:
+                    pos = [x * cellSize + gridPos[0], y * cellSize + gridPos[1]]
+                    self.textToScreen(window, str(num), pos)
+
     def drawSelection(self, window, pos):
-        pygame.draw.rect(window, LIGHTORANGE, ((pos[0] * cellSize) + gridPos[0], (pos[1] * cellSize) + gridPos[1],
-                                             cellSize, cellSize))
+        pygame.draw.rect(window, LIGHTORANGE, ((pos[0] * cellSize) + gridPos[0], (pos[1] * cellSize) + gridPos[1], cellSize, cellSize))
 
     def drawGrid(self, window):
         pygame.draw.rect(window, BLACK, (gridPos[0], gridPos[1], WIDTH-150, HEIGHT-150), 2)
-        # vertical lines
+
         for x in range(9):
-            if x % 3 == 0:
-                pygame.draw.line(window, BLACK, (gridPos[0] + x * cellSize, gridPos[1]),
-                                 (gridPos[0] + x * cellSize, gridPos[1] + 450), 2)
-            else:
-                pygame.draw.line(window, BLACK, (gridPos[0] + x * cellSize, gridPos[1]),
-                                 (gridPos[0] + x * cellSize, gridPos[1]+450))
-        # horizontal lines
-        for x in range(9):
-            if x % 3 == 0:
-                pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1] + x * cellSize),
-                                 (gridPos[0] + 450, gridPos[1] + x * cellSize), 2)
-            else:
-                pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1] + x * cellSize),
-                                 (gridPos[0] + 450, gridPos[1] + x * cellSize))
+            pygame.draw.line(window, BLACK, (gridPos[0] + x * cellSize, gridPos[1]), (gridPos[0] + x * cellSize, gridPos[1] + 450), 2 if x % 3 == 0 else 1)
+            pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1] + x * cellSize), (gridPos[0] + 450, gridPos[1] + x * cellSize), 2 if x % 3 == 0 else 1)
 
     def mouseOnGrid(self):
         if self.mousePos[0] < gridPos[0] or self.mousePos[1] < gridPos[1]:
@@ -92,4 +94,38 @@ class App:
 
     def loadButtons(self):
         self.playingButtons.append(Button(20, 40, 100, 40))
+
+    def textToScreen(self, window, text, pos, color=BLACK):
+        font = self.font.render(text, False, color)
+        fontWidth = font.get_width()
+        fontHeight = font.get_height()
+        pos[0] += (cellSize - fontWidth) // 2
+        pos[1] += (cellSize - fontHeight) // 2
+        window.blit(font, pos)
+
+    def load(self):
+        self.loadButtons()
+
+        # Setting locked cells
+        for y, row in enumerate(self.grid):
+            for x, num in enumerate(row):
+                if num != 0:
+                    self.lockedCells.append([x, y])
+
+    def shadeLockedCells(self, window, locked):
+        for cell in locked:
+            pygame.draw.rect(window, CORNSILK, (cell[0] * cellSize + gridPos[0], cell[1] * cellSize + gridPos[1], cellSize, cellSize))
+
+    def isInt(self, str):
+        try:
+            int(str)
+            return True
+        except:
+            return False
+
+
+
+
+
+
 
--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+# Dimensions
 WIDTH = 600
 HEIGHT = 600
 
@@ -5,11 +6,26 @@ HEIGHT = 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 LIGHTORANGE = (250, 171, 83)
+CORNSILK = (255, 248, 220)
+
+# Fonts
+font = 'lato'
 
 # Boards
-testBoard = [[0 for x in range(9)] for x in range(9)]
+testBoard1 = [[0 for x in range(9)] for x in range(9)]
+testBoard2 = board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
 
-# Positions and sizes
+# Positions and Sizes
 gridPos = (75, 100)
 cellSize = 50
 gridSize = cellSize * 9
